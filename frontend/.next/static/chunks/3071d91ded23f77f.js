7d58b694e05617d8e2e35e706110a63c90e956f0
Khushi Kumari <khushikumari8204@gmail.com>
2025-10-23 19:47:58 +0530

    basic frontend done

---
+++ frontend/.next/static/chunks/3071d91ded23f77f.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/9a11e9deff7bb61a.js",
+  "static/chunks/turbopack-91d49663a0acabb5.js"
+])
---
+++ frontend/.next/static/chunks/a40005e5bd1741db.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/9a11e9deff7bb61a.js",
+  "static/chunks/turbopack-1e1b286c62797128.js"
+])
